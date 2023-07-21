23eb0fd58defd16432dd3fe010cf6079b4230277
Samuel Adetunji <guysnacho2@gmail.com>
2023-07-20 23:58:18 -0500

    Space login appropriately

--- app/screens/LandingScreen.tsx
+++ app/screens/LandingScreen.tsx
@@ -4,10 +4,9 @@ import { AppStackScreenProps } from "app/navigators"
 import { useSafeAreaInsetsStyle } from "app/utils/useSafeAreaInsetsStyle"
 import { observer } from "mobx-react-lite"
 import React, { FC } from "react"
-import { ViewStyle } from "react-native"
 import welcomeLogo from "../../assets/images/logo-filled.png"
 
-import { View, Text, Image, Button, YStack } from "tamagui"
+import { Button, Image, Text, XStack, YStack } from "tamagui"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
@@ -21,28 +20,24 @@ export const LandingScreen: FC<LandingScreenProps> = observer(function LandingSc
   // Pull in navigation via hook
   // const navigation = useNavigation()
   return (
-    <Screen style={$topContainerInsets} preset="scroll">
-      <View>
-        <Image source={welcomeLogo} marginHorizontal="auto" />
-      </View>
-      <Text color="forestgreen" textAlign="center" marginVertical={5} fontSize={5}>
-        Listen with us 🎵
-      </Text>
-      <YStack justifyContent="space-around" alignContent="center" space>
-        <Button alignSelf="center" size="$5">
-          Login
-        </Button>
-        <Button alignSelf="center" size="$5">
-          Sign Up
-        </Button>
-        <Button alignSelf="center" size="$5">
-          Try it Out
-        </Button>
-      </YStack>
+    <Screen style={$topContainerInsets}>
+      <XStack height="80%" marginVertical="auto" justifyContent="space-around" alignSelf="center">
+        <YStack display="flex" justifyContent="space-around" alignContent="center" space>
+          <Image source={welcomeLogo} marginHorizontal="auto" />
+          <Text color="forestgreen" textAlign="center" marginVertical={5} fontSize="$8">
+            Listen with us 🎵
+          </Text>
+          <Button alignSelf="center" size="$5">
+            Login
+          </Button>
+          <Button alignSelf="center" size="$5">
+            Sign Up
+          </Button>
+          <Button alignSelf="center" size="$5">
+            Try it Out
+          </Button>
+        </YStack>
+      </XStack>
     </Screen>
   )
 })
-
-const $root: ViewStyle = {
-  flex: 1,
-}
